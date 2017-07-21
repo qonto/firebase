0e0d124253b5711fe04a1903a10084a5db59d1cb
Maxime <maxime.dev@gmail.com>
2017-07-21 10:47:01 +0200

    not raise NotRegistered

--- lib/firebase_messenger/api/notification.rb
+++ lib/firebase_messenger/api/notification.rb
@@ -5,7 +5,7 @@ module FirebaseMessenger
         response = conn('send', params).post
         return response.json if ok?(response)
         error = FirebaseMessenger::BaseError.new(200, response.results.to_s)
-        raise error, error.message
+        raise error, error.message unless error.message.to_s.include?('NotRegistered')
       end
     end
   end
